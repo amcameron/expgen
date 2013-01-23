60de06b2f6d597acdcb4d8d9268a56adb8e6a4ea
Andrew Cameron <andrew@acameron.org>
2013-01-23 17:35:21 -0500

    remove Exposure class; add functions for parsing exposure strings; fix ISO bug

--- expgen.py
+++ expgen.py
@@ -4,16 +4,27 @@ import random
 import exposure
 
 LUCK_SCALE_FACTOR = 2
+MIN_ISO = exposure.iso_to_ev('1600 ISO')
+MAX_ISO = exposure.iso_to_ev('100 ISO')
+MIN_APERTURE = exposure.fnumber_to_ev('f/1.4')
+MAX_APERTURE = exposure.fnumber_to_ev('f/22.0')
 
-def synthesize(exp, luck, focal=6):
+def synthesize(exp, luck, focal):
+    # TODO: use bounds during synthesis so we can't generate invalid values.
     random.seed()
-    evTarget = exp + random.uniform(-LUCK_SCALE_FACTOR*luck,
-            LUCK_SCALE_FACTOR*luck)
-    evIso = random.uniform(5, 9.1)
-    evShutter = focal + random.uniform(-LUCK_SCALE_FACTOR*luck,
-            LUCK_SCALE_FACTOR*luck)
+    evFocal = exposure.seconds_to_ev('1/' + focal + 's')
+    evShutter = evFocal + random.uniform(
+            -LUCK_SCALE_FACTOR*luck, LUCK_SCALE_FACTOR*luck)
+    evTarget = exp + random.uniform(
+            -LUCK_SCALE_FACTOR*luck, LUCK_SCALE_FACTOR*luck)
+    # evTarget = evShutter + evAperture + evIso
+    # evTarget - evShutter = evAperture + evIso
+    remainder = evTarget - evShutter
+    isoLower = max(MIN_ISO, remainder - MAX_APERTURE)
+    isoUpper = min(MAX_ISO, remainder - MIN_APERTURE)
+    evIso = random.uniform(isoLower, isoUpper)
     evAperture = evTarget - evIso - evShutter
-    return exposure.Exposure(iso=evIso, shutter=evShutter, aperture=evAperture)
+    return evShutter, evAperture, evIso
 
 if __name__ == "__main__":
     usage = "usage: %prog [options]"
@@ -21,7 +32,7 @@ if __name__ == "__main__":
     parser.add_option(
             '-e', '--exposure',
             action='store', type='int', dest='exp', default=exposure.SUNNY,
-            help='Exposure value in EV (Sunny 16: ~{0} EV)'.format(
+            help='Exposure value in EV (Sunny 16: ~{0:.0f} EV)'.format(
                 exposure.SUNNY))
     parser.add_option(
             '-l', '--luck',
@@ -29,11 +40,10 @@ if __name__ == "__main__":
             help='How lucky are you feeling? (variability)')
     parser.add_option(
             '-f', '--focal',
-            action='store', type='int', dest='focal', default=50,
+            action='store', type='string', dest='focal', default='50',
             help='Lens focal length (for handheld limit)')
     (options, args) = parser.parse_args()
 
-    result = synthesize(options.exp, options.luck)
-    while result._evAperture > 11 and result._evAperture >= 1:
-        result = synthesize(options.exp, options.luck)
-    print result
+    evShutter, evAperture, evIso = synthesize(
+            options.exp, options.luck, options.focal)
+    print exposure.evs_to_exposure(evShutter, evAperture, evIso)
--- exposure.py
+++ exposure.py
@@ -1,42 +1,83 @@
 """Exposure: a combination of settings determining photographic exposure."""
-from math import sqrt
+from math import log, sqrt
+import re
+
+# Decimal matching pattern, e.g. "4", "2.", "35.5", "0.0051", etc.
+_number_pattern = r'(\d+(?:\.\d*)?|\.\d+)'
+
+def ev_to_seconds(evShutter):
+    """Convert EV to string representing seconds or fractions of a second."""
+    # Based on EV 0 = 1 s.  If brighter: more light, faster speed, higher EV.
+    # Represent faster shutter speeds fractionally, like a camera.
+    power = 2**abs(evShutter)
+    seconds = '%d s' % power #if evShutter <= 0 else '1/%d s'
+    if evShutter > 0:
+        seconds = '1/' + seconds
+    return seconds
+
+# Pattern to match seconds or fractional seconds input.
+_reSeconds = re.compile(
+        _number_pattern + r' ?s' + # number, optional space, 's'
+        r'|' + # or...
+        r'1/' + _number_pattern + r' ?s') # '1/', number, optional space, 's'
+
+def seconds_to_ev(seconds):
+    """Convert string representing seconds to EV.
+
+    Accepted formats are "#s", "# s", "1/#s", "1/# s"."""
+    match = _reSeconds.match(seconds)
+    if not match:
+        raise ValueError("Unable to parse seconds value.", seconds)
+    if seconds.startswith('1/'):
+        sec = 1/float(match.group(2))
+    else:
+        sec = float(match.group(1))
+    stops = -log(sec, 2)
+    return stops
+
+def ev_to_fnumber(evAperture):
+    """Convert EV to string representing aperture (f-number)."""
+    # Based on EV 0 = f/1.0
+    fnum = sqrt(2)**evAperture
+    return 'f/{0:.1f}'.format(fnum)
+
+# Pattern to match aperture values.
+_reFnumber = re.compile(r'f/?' + _number_pattern)
+
+def fnumber_to_ev(aperture):
+    """Convert string representing aperture (e.g. "f/1.4", "f2.8") to EV."""
+    match = _reFnumber.match(aperture)
+    if not match:
+        raise ValueError("Unable to parse aperture value.", aperture)
+    fnum = float(match.group(1))
+    stops = log(fnum, sqrt(2))
+    return stops
+
+def ev_to_iso(evIso):
+    """Convert EV to string representing sensitivity (ISO)."""
+    # Assumes EV 0 = ISO 3
+    return '{0:.0f} ISO'.format(3*2**-evIso)
+
+# Pattern to match ISO values.
+_reIso = re.compile(r'(\d+)(?: [iI][sS][oO])?')
+
+def iso_to_ev(iso):
+    """Convert string representing ISO (e.g. "200 ISO", "1600") to EV."""
+    match = _reIso.match(iso)
+    if not match:
+        raise ValueError("Unable to parse ISO value.", iso)
+    num = float(match.group(1))
+    stops = -log(num/3, 2)
+    return stops
+
+def evs_to_exposure(evShutter, evAperture, evIso):
+    """Convert EVs to string representing exposure."""
+    sExp = ', '.join((
+            ev_to_seconds(evShutter),
+            ev_to_fnumber(evAperture),
+            ev_to_iso(evIso)))
+    return sExp + ' ({0:.0f} EV)'.format(evShutter + evAperture + evIso)
 
 # Sunny 16 rule says that at f/16 (EV +8), use shutter = 1/iso.
 # E.g., 100 ISO (EV +5) and 1/100 s (EV +6.67). 8+5+7 = 20.
-SUNNY = 20
-
-class Exposure(object):
-    # Exposure values are stored in EV
-    _evShutter = 0
-    _evAperture = 0
-    _evIso = 0
-
-    def __init__(self, shutter=0, aperture=0, iso=0):
-        self._evShutter = shutter
-        self._evAperture = aperture
-        self._evIso = iso
-
-    def __repr__(self):
-        return ', '.join([ev_to_seconds(self._evShutter),
-            ev_to_fnumber(self._evAperture),
-            ev_to_iso(self._evIso)]) + \
-                    ' ({0:.0f} EV)'.format(
-                    self._evShutter + self._evAperture + self._evIso)
-
-def ev_to_seconds(shutter):
-    """Convert EV to string representing seconds or fractions of a second."""
-    # Based on EV 0 = 1 s
-    power = 2**abs(shutter)
-    fmt = '%d s' if shutter <= 0 else '1/%d s'
-    return fmt % power
-
-def ev_to_fnumber(aperture):
-    """Convert EV to string representing relative aperture (f-number)."""
-    # Based on EV 0 = f/1.0
-    fnum = sqrt(2)**aperture
-    return 'f/{0:.1f}'.format(fnum)
-
-def ev_to_iso(iso):
-    """Convert EV to string representing sensitivity (ISO)."""
-    # Assumes EV 0 = ISO 3
-    return '{0:.0f} ISO'.format(3*2**iso)
+SUNNY = fnumber_to_ev('f/16') + seconds_to_ev('1/100 s') + iso_to_ev('100 ISO')
