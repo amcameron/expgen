653ed841a3dd8c9775495001d09ab5fb35e6ceff
Andrew Cameron <andrew@acameron.org>
2013-01-28 12:04:58 -0500

    add debugging logs to webapp

--- webapp.py
+++ webapp.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+import logging
 from flask import Flask, render_template, request
 from expgen import MIN_EXPOSURE, MAX_EXPOSURE, MIN_ISO, MAX_ISO, \
         MIN_APERTURE, MAX_APERTURE, MIN_SHUTTER, MAX_SHUTTER, \
         synthesize
-from exposure import ev_to_seconds, ev_to_fnumber, ev_to_iso
+from exposure import ev_to_seconds, ev_to_fnumber, ev_to_iso, evs_to_exposure
 app = Flask(__name__)
+app.logger.setLevel(logging.DEBUG)
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
@@ -16,7 +18,10 @@ def form():
                 isoMin=MIN_ISO, isoMax=MAX_ISO)
     elif request.method == 'POST':
         evTarget = float(request.form['target'])
+        app.logger.debug('form activated, evTarget: {}'.format(evTarget))
         evS, evA, evI = synthesize(float(evTarget), 1, '50')
+        app.logger.debug('values synthesized: {}'.format(
+            evs_to_exposure(evShutter=evS, evAperture=evA, evIso=evI)))
         return render_template('form.html',
                 expMin=MIN_EXPOSURE, expMax=MAX_EXPOSURE,
                 shutterMin=MIN_SHUTTER, shutterMax=MAX_SHUTTER,
